3f83a970f94f0dd18e7654bdaf8f64811d50bf6e
Yusuke Izawa <yuizalp@gmail.com>
2017-05-01 21:04:31 +0900

    create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+def f(a, b):
+    if b % 64 == 41:
+        return a - b
+    else:
+        return a + b
+
+def strange_sum(n):
+    result = 0
+    while n >= 0:
+        result = f(result, n)
+        n -= 1
+    return result
+
+print strange_sum(10)
